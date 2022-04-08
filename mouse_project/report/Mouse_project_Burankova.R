f3e4e5d7009177bb12117d70d08a7f043105cf2b
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2022-04-08 20:34:44 +0200

    2nd task

--- mouse_project/report/Mouse_project_Burankova.R
+++ mouse_project/report/Mouse_project_Burankova.R
@@ -123,10 +123,197 @@ lapply(num_cols_names, function_name)
 mouse %>% 
   filter(!is.na(BDNF_N)) %>% 
   group_by(class) %>% 
-  summarise(n = n())
+  summarise(n_BDNF_N = n())
 
 ggplot(mouse, aes(BDNF_N, color=class)) +
   geom_density() +
-  theme_bw()
+  theme_bw() +
+  labs(title="Boxplot for Protein in each class",
+       x ="Class")
+
+ggplot(mouse, aes(y = BDNF_N, x = class)) +
+  geom_violin() +
+  stat_boxplot(geom ='errorbar', width=0.7) +
+  geom_boxplot(width=0.2) +
+  stat_summary(geom="text", fun=quantile,
+               aes(label=sprintf("%1.1f", ..y..)),
+               position=position_nudge(x=0.45), size=3.5) +
+  theme_bw() +
+  labs(title="Boxplot for Protein in each class",
+       x ="Class")
+
+## выбросы для каждого класса
+
+quants <- c(0, 0.25, 0.50, 0.75, 1)
+
+### 1
+quant <- mouse_BDNF_N %>% 
+  filter(class == 'c-CS-m') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- mouse_BDNF_N %>% filter(class == 'c-CS-m' & !BDNF_N > quant[4,1] + iqr)
+mouse_wo_outl <- mouse_wo_outl %>% filter(class == 'c-CS-m' & !BDNF_N < quant[2,1] - iqr)
+
+### 2
+quant <- mouse_BDNF_N %>% 
+  filter(class == 'c-CS-s') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- rbind(mouse_wo_outl, mouse_BDNF_N %>% 
+                         filter(class == 'c-CS-s' & !(BDNF_N < (quant[2,1] - iqr) | BDNF_N > quant[4,1] + iqr)))
+
+### 3
+quant <- mouse_BDNF_N %>% 
+  filter(class == 'c-SC-m') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- rbind(mouse_wo_outl, mouse_BDNF_N %>% 
+                         filter(class == 'c-SC-m' & !(BDNF_N < (quant[2,1] - iqr) | BDNF_N > quant[4,1] + iqr)))
+
+### 4
+quant <- mouse_BDNF_N %>% 
+  filter(class == 'c-SC-s') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- rbind(mouse_wo_outl, mouse_BDNF_N %>% 
+                         filter(class == 'c-SC-s' & !(BDNF_N < (quant[2,1] - iqr) | BDNF_N > quant[4,1] + iqr)))
+
+### 5
+quant <- mouse_BDNF_N %>% 
+  filter(class == 't-CS-m') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- rbind(mouse_wo_outl, mouse_BDNF_N %>% 
+                         filter(class == 't-CS-m' & !(BDNF_N < (quant[2,1] - iqr) | BDNF_N > quant[4,1] + iqr)))
 
 
+### 6
+quant <- mouse_BDNF_N %>% 
+  filter(class == 't-CS-s') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- rbind(mouse_wo_outl, mouse_BDNF_N %>% 
+                         filter(class == 't-CS-s' & !(BDNF_N < (quant[2,1] - iqr) | BDNF_N > quant[4,1] + iqr)))
+
+### 7
+quant <- mouse_BDNF_N %>% 
+  filter(class == 't-SC-m') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- rbind(mouse_wo_outl, mouse_BDNF_N %>% 
+                         filter(class == 't-SC-m' & !(BDNF_N < (quant[2,1] - iqr) | BDNF_N > quant[4,1] + iqr)))
+
+
+### 8
+quant <- mouse_BDNF_N %>% 
+  filter(class == 't-SC-s') %>% 
+  select(BDNF_N) %>%  
+  apply(., 2, quantile, probs = quants) 
+
+iqr <- (quant[4,1] - quant[2,1]) * 1.75
+
+mouse_wo_outl <- rbind(mouse_wo_outl, mouse_BDNF_N %>% 
+                         filter(class == 't-SC-s' & !(BDNF_N < (quant[2,1] - iqr) | BDNF_N > quant[4,1] + iqr)))
+
+
+
+
+
+## plot
+
+ggplot(mouse_wo_outl, aes(y = BDNF_N, x = class)) +
+  geom_violin() +
+  stat_boxplot(geom ='errorbar', width=0.7) +
+  geom_boxplot(width=0.2) +
+  theme_bw() +
+  labs(title="Boxplot for BDNF_N Protein in each class",
+       x ="Class")
+
+
+mouse_wo_outl %>% 
+  group_by(class) %>% 
+  summarise(n_BDNF_N = n())
+
+##SW
+mouse_wo_outl$class = as.factor(mouse_wo_outl$class)
+
+mod_BDNF <- lm(BDNF_N ~ class, data = mouse_wo_outl)
+summary(mod_BDNF)
+
+ggplot(mouse_wo_outl, aes(x = mod_BDNF$residuals)) +
+  geom_histogram(bins=30, fill = 'blue', alpha = 0.5, color = 'black') +
+  theme_bw() +
+  labs(title = 'Histogram of Residuals', 
+       x = 'Residuals', 
+       y = 'Frequency')
+
+spariro_res <- shapiro.test(mod_BDNF$residuals)
+
+ggplot(data = mouse_wo_outl,
+       aes(sample = mod_BDNF$residuals)) +
+  geom_qq() +
+  theme_bw() +
+  geom_qq_line(colour = "red") +
+  labs(title = "Quantile plot of residuals")
+
+ggplot(mod_BDNF, 
+       aes(x = class, y = .stdresid)) + 
+  geom_violin() + 
+  theme_bw() +
+  geom_boxplot(width = 0.1) + 
+  labs(title = "Plot of residuals",
+       x = "Experiment class",
+       y = 'St.d. of the residuals')
+
+### ANOVA
+library(car)
+anova_test_BDNF <- Anova(mod_BDNF)
+anova_test_BDNF
+
+library(multcomp)
+post_hoch_BDNF <- glht(mod_BDNF, linfct = mcp(class = "Tukey"))
+result_posthoch <- summary(post_hoch_BDNF)
+
+result_posthoch$test$tstat['c-CS-s - c-CS-m']
+
+
+# Попробовать построить линейную модель, способную предсказать уровень продукции белка ERBB4_N 
+# на основании данных о других белках в эксперименте (15 баллов) 
+# -- провести диагностику полученной линейной модели 
+# -- объяснить, почему это является хорошим/не хорошим решением 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
